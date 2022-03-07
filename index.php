06bebff445d6eb396628b16f920fa22dd093114b
Naima Hassan <naima.hassan434@gmail.com>
2022-03-07 13:33:03 +0000

    index page

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php 
-require('top.php');
+   require('top.php');
 ?>
-
-<?php
+<div class="content pb-0">
+            <div class="orders">
+               <div class="row">
+                  <div class="col-xl-12">
+                     <div class="card">
+                        <div class="card-body">
+                           <h4 class="box-title">Department Master </h4>
+						   <h4 class="box_title_link"><a href="add_department.php">Add Department</a> </h4>
+                        </div>
+                        <div class="card-body--">
+                           <div class="table-stats order-table ov-h">
+                              <table class="table ">
+                                 <thead>
+                                    <tr>
+                                       <th width="5%">S.No</th>
+                                       <th width="5%">ID</th>
+                                       <th width="70%">Department Name</th>
+                                       <th width="20%"></th>
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+                                    <tr>
+                                       <td>Serial</td>
+                                       <td>Id</td>
+                                       <td>Department Name</td>
+                                       <td></td>
+                                    </tr>
+                                 </tbody>
+                              </table>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+		  </div>
+<!-- <?php
 require('footer.php');
-?>
+?> -->
